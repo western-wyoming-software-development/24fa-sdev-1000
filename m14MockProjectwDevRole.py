e9a796287699a6741638a51464b6938eb658347c
Tammy Robertson Callahan <80910439+DrTammyRobertson@users.noreply.github.com>
2024-10-24 09:49:18 -0700

    Add files via upload

---
+++ m14MockProjectwDevRole.py
@@ -0,0 +1,9 @@
+#
+# Dr. Tammy Robertson
+# 10/24/2024
+# Mock Project w/Developer Role
+# SDEV 1000
+#
+
+# Let's practice our new Git/GitHub skills.
+print("Hello Class! Respond back (using Python code) to this program with your own greeting. Be sure to identify who you are, too!")
